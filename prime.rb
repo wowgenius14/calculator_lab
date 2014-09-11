9595445f3f6209536e84cd1677f49f98e0f52488
wowgenius14 <jh46728@students.westport.k12.ct.us>
2014-09-11 09:10:12 -0400

    Update prime.rb

--- prime.rb
+++ prime.rb
@@ -3,18 +3,17 @@ puts "Please enter a number to see if it's a prime number"
 m = gets.chomp
 n = m.to_i
 if n%2 ==0
-puts "It is not a prime number"
+  puts "It is not a prime number"
 
 else
-i = 3
+  i = 3
 while i * i  <= n
-if n%i ==0
-puts "It is not a prime number"
-
-exit
-break
-end
-i+=2
-end
-puts "It's a prime number"
+  if n%i ==0 #Checks the if the odd numbers are prime number
+    puts "It is not a prime number"
+    exit#CLoses program to avoid to different outputs
+    break#Stops the loop from continuing
+    end
+    i+=2
+  end
+  puts "It's a prime number"
 end
