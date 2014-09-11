0473417bf2a151eb6051e7052c08cbe6bfe46da0
wowgenius14 <jh46728@students.westport.k12.ct.us>
2014-09-11 08:56:02 -0400

    Update fact.rb

--- fact.rb
+++ fact.rb
@@ -1,9 +1,9 @@
 #factorials
 puts " Enter a number to find its factorial"
 input = gets.to_i
-sum = 1
+product = 1
 while ( input  >1)
-sum  *= input
-input -=1
+  product  *= input
+  input -=1
 end
-puts sum.to_s
+puts product.to_s
